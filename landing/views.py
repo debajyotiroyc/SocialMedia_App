637807ceac485844de2347e86f41a4fe337d9f38
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-11-04 17:32:40 +0530

    Add files via upload

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+class Index(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'landing/index.html')
+
+
+
+
